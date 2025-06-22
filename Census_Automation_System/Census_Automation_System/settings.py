f4b7018c69c3ff0174f0d5928a45be8d1a68273e
taki tamim <takitamim2001@gmail.com>
2025-06-22 12:21:20 +0600

    Switch to PostgreSQL with auto migration + superuser creation

--- Census_Automation_System/Census_Automation_System/settings.py
+++ Census_Automation_System/Census_Automation_System/settings.py
@@ -15,7 +15,9 @@ Django settings for Census_Automation_System project.
 """
 
 from pathlib import Path
+import dj_database_url
 import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -77,10 +79,11 @@ WSGI_APPLICATION = "Census_Automation_System.wsgi.application"
 
 # Database
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
 
 # Password validation
--- Census_Automation_System/census/apps.py
+++ Census_Automation_System/census/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class CensusConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "census"
+
+    def ready(self):
+        from . import auto_init
+        auto_init.initialize()
